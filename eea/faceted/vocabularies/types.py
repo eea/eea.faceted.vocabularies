c1d5f34c2e72e9747c8b8bb6dbc1a5289979dc98
Razvan Chitu <razvan3895@yahoo.com>
2015-06-22 15:18:55 +0300

    Fix pylint issues

--- eea/faceted/vocabularies/types.py
+++ eea/faceted/vocabularies/types.py
@@ -18,8 +18,8 @@ class PortalTypesVocabulary(object):
         ttool = getToolByName(context, 'portal_types', None)
         if ttool is None:
             return None
-        items = [ (ttool[t].Title(), t)
-                  for t in ttool.listContentTypes() ]
+        items = [(ttool[t].Title(), t)
+                  for t in ttool.listContentTypes()]
         items.sort(key=operator.itemgetter(0), cmp=compare)
         items = [SimpleTerm(i[1], i[1], i[0]) for i in items]
         return SimpleVocabulary(items)
